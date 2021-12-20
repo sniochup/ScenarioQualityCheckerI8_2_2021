16e91103ed139d437ff450778f470178749f13c5
sniochup <snioszekpawel@gmail.com>
2021-12-20 20:16:45 +0100

    Rest controller comments for javadocs

--- src/main/java/pl/put/poznan/sqc/rest/ScenarioQualityCheckerController.java
+++ src/main/java/pl/put/poznan/sqc/rest/ScenarioQualityCheckerController.java
@@ -4,13 +4,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.sqc.scenario.Scenario;
 
-
+/**
+ * Rest controller class
+ */
 @RestController
 @RequestMapping("/sqc")
 public class ScenarioQualityCheckerController {
 
     private static final Logger logger = LoggerFactory.getLogger(ScenarioQualityCheckerController.class);
 
+    /**
+     * steps-count endpoint service
+     *
+     * @param json - scenario received in request body
+     * @return count of steps in scenario in json format
+     */
     @GetMapping(path = "/steps-count", produces = "application/json")
     public Scenario getStepsCount(@RequestBody Scenario json) {
 
@@ -20,6 +28,12 @@ public class ScenarioQualityCheckerController {
         return json;
     }
 
+    /**
+     * keyword endpoint service
+     *
+     * @param json - scenario received in request body
+     * @return number of steps in the scenario which begin with the keyword (json format)
+     */
     @GetMapping(path = "/keyword", produces = "application/json")
     public Scenario getKeyword(@RequestBody Scenario json) {
 
@@ -29,6 +43,12 @@ public class ScenarioQualityCheckerController {
         return json;
     }
 
+    /**
+     * no-actor endpoint service
+     *
+     * @param json - scenario received in request body
+     * @return list of steps in the scenario which not begin with the actor name (json format)
+     */
     @GetMapping(path = "/no-actor", produces = "application/json")
     public Scenario getNoActor(@RequestBody Scenario json) {
 
@@ -38,6 +58,12 @@ public class ScenarioQualityCheckerController {
         return json;
     }
 
+    /**
+     * send-scenario endpoint service
+     *
+     * @param json - scenario received in request body
+     * @return numbered scenario (json format)
+     */
     @GetMapping(path = "/send-scenario", produces = "application/json")
     public Scenario getSendScenario(@RequestBody Scenario json) {
 
@@ -48,5 +74,3 @@ public class ScenarioQualityCheckerController {
     }
 
 }
-
-
